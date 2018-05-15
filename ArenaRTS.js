73a47d4fd01e710550c7aa40c36e48a322520736
Mr-DKKing <33702377+Mr-DKKing@users.noreply.github.com>
2018-05-15 10:00:33 -0400

    Update ArenaRTS.js

--- ArenaRTS.js
+++ ArenaRTS.js
@@ -1,5 +1,14 @@
 let ctx = canvas.getContext("2d");
 let canvas = document.getElementbyId("")
-function PlayButton() {
+pressplaybutton = false;
+function PlayGame() {
   pressplaybutton = true;
+}
+
+function pressedKey(event) {
+  var key = event.keyCode
+  function animate() {
+    Sprite = new Image();
+    Sprite.src
+  }
 }
